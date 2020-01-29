71ce71a149ade71826f11ea1001fde0055903bf2
Adolfo Zuniga <adolfozuniga0@gmail.com>
2020-01-29 06:38:27 +0100

    Added STATIC_ROOT

--- bzdev/settings.py
+++ bzdev/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-DISABLE_COLLECTSTATIC=1
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
